a1a4246ecb06e361c559b48189b2052a62be60b6
ariaazadparvar1385 <ariaazadparvar1385@gmail.com>
2024-11-18 18:54:33 +0330

    Add files via upload

---
+++ Untitled1.cpp
@@ -0,0 +1,22 @@
+#include<iostream>;
+using namespace std;
+main(){
+	float x;
+	cin>>x;
+	if(15<x&&x>20){
+		
+		cout<<"A"<<endl;
+		cout<<"very good";
+	}
+else if(10<x&&x<15){
+
+		cout<<"B"<<endl;
+		cout<<"Try it";
+	}
+	else if(0<x&&x<10){
+		
+		cout<<"C"<<endl;
+		cout<<"BAD";
+	}
+	
+}
