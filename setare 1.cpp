7257823977b469725cfb39d6906bd69a6874463b
ariaazadparvar1385 <ariaazadparvar1385@gmail.com>
2024-12-02 20:29:43 +0330

    Add files via upload

---
+++ setare 1.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+main(){
+	int i,a;
+	for(i=1;i<=5;i++){
+		for(a=1;a<=i;a++)
+	cout<<"*";
+	cout<<"\n";}
+	
+}
---
+++ setare2.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+main(){
+	int i,a;
+	for(i=10;i>=1;i--){
+		for(a=1;a<=i;a++)
+	cout<<"* ";
+	cout<<"\n";}
+	
+}
